9a115095c155bea4a36e6cab7dbbab2ecc5978ff
ishi32 <ishi32@unremoted.com>
2022-06-06 16:48:47 +0900

    index.tsx swiper added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
